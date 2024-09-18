2cf7a4aa75ee9e20c3e423361d167ad42bc1c4cf
lxhy-op <bhardlak@sheridancollege.ca>
2024-09-18 12:36:18 -0400

    uhzgdvfy

---
+++ src/week4/software/design/Week4SoftwareDesign.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package week4.software.design;
+
+/**
+ *
+ * @author User
+ */
+public class Week4SoftwareDesign {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //comment here
+        //khzsgzdgfus
+    }
+    
+}
